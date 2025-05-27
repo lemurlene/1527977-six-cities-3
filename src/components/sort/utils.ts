fc5132906fed39518e1c5fd4e79292377663bf9c
Gorelikova Elena <lenagorelikova4560@gmail.com>
2025-05-27 09:23:17 +0300

    fix: исправлет toSorted на sort

--- src/components/sort/utils.ts
+++ src/components/sort/utils.ts
@@ -3,17 +3,18 @@ import { SortTypes } from './const';
 import { CardType } from '../../const/type';
 
 const sortBy = {
-  [SortTypes.Popular]: (offers: CardType[]):CardType[] =>
+  [SortTypes.Popular]: (offers: CardType[]): CardType[] =>
     offers,
-  [SortTypes.Low]: (offers: CardType[]):CardType[] =>
-    offers.toSorted((a, b) => a.price - b.price),
-  [SortTypes.High]: (offers: CardType[]):CardType[] =>
-    offers.toSorted((a, b) => b.price - a.price),
-  [SortTypes.Top]: (offers: CardType[]):CardType[] =>
-    offers.toSorted((a, b) => b.rating - a.rating),
+  [SortTypes.Low]: (offers: CardType[]): CardType[] =>
+    [...offers].sort((a, b) => a.price - b.price),
+  [SortTypes.High]: (offers: CardType[]): CardType[] =>
+    [...offers].sort((a, b) => b.price - a.price),
+  [SortTypes.Top]: (offers: CardType[]): CardType[] =>
+    [...offers].sort((a, b) => b.rating - a.rating),
 };
 
-const sortOffers = (offers: CardType[], sortType: SortType):CardType[] =>
+const sortOffers = (offers: CardType[], sortType: SortType): CardType[] =>
   sortBy[sortType](offers);
 
 export { sortOffers };
+
