fb2efefeff51f92a37d43b21ab7847fc02c50536
Gorelikova Elena <91557439+lemurlene@users.noreply.github.com>
2025-11-03 09:52:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/1527977-six-cities-3/',
+  build: {
+    outDir: 'docs',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
